8cd206e7b2970e16be0aabe8459e3296f3a69efd
feliperibeiroa <feliperibeirots@gmail.com>
2025-06-05 11:47:58 -0300

    feita a parte de previsao

---
+++ NimbusApi/Services/PrevisaoService.cs
@@ -0,0 +1,37 @@
+﻿namespace NimbusApi.Services
+{
+    public class PrevisaoService
+    {
+        private readonly PrevisaoRepository _previsaoRepository;
+
+        public PrevisaoService(PrevisaoRepository previsaoRepository)
+        {
+            _previsaoRepository = previsaoRepository;
+        }
+
+        public async Task<IEnumerable<Previsao>> GetAllAsync()
+        {
+            return await _previsaoRepository.GetAllAsync();
+        }
+
+        public async Task<Previsao?> GetByIdAsync(int id)
+        {
+            return await _previsaoRepository.GetByIdAsync(id);
+        }
+
+        public async Task<Previsao> AddAsync(Previsao previsao)
+        {
+            return await _previsaoRepository.AddAsync(previsao);
+        }
+
+        public async Task<Previsao> UpdateAsync(Previsao previsao)
+        {
+            return await _previsaoRepository.UpdateAsync(previsao);
+        }
+
+        public async Task DeleteAsync(Previsao previsao)
+        {
+            await _previsaoRepository.DeleteAsync(previsao);
+        }
+    }
+}
